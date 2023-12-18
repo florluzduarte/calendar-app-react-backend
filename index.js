80f06a79d9b8f1bb704779c892206311a940a89c
Florencia Luz Duarte <florluzduarte@gmail.com>
2023-12-18 16:03:57 -0300

    Add --> Event endpoints

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use(express.json());
 
 //Configuración de rutas
 app.use("/api/auth", require("./routes/auth"));
+app.use("/api/events", require("./routes/events"));
 
 //Escuchar peticiones
 app.listen(process.env.PORT, () => {
